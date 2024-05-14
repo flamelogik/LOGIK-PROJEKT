c8a44e1ce97e50cdfaab139d3620736a853b6e0a
phil-man-git-hub <phil_man@mac.com>
2024-05-14 14:29:02 -0700

    2024-05-14 started utility template directories

---
+++ create_logik_projekt/utility/template/bash/functions/function_00-docstring.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# -------------------------------------------------------------------------- #
+
+# File Name:        function_00-docstring.sh
+# Version:          2.0.4
+# Language:         bash script
+# Flame Version:    2025.x
+# Author:           Phil MAN - phil_man@mac.com
+# Toolset:          MAN_MADE_MATERIAL: LOGIK-PROJEKT
+# Modified:         2024-05-03
+# Modifier:         Phil MAN - phil_man@mac.com
+
+# Description:      This program contains function(s) that are used to
+#                   create new logik projekts.
+
+# Installation:     Copy the 'LOGIK-PROJEKT' repo to your GitHub directory,
+#                   e.g. '/home/$USER/workspace/GitHub'
+
+# Changelist:       The full changelist is at the end of this document.
+
+# -------------------------------------------------------------------------- #
+
+# ========================================================================== #
+# This section identifies and locates this script when executed.
+# ========================================================================== #
+
+# Get the base name of the script.
+script_name=$(basename "$0")
+
+# Get the full path to the script using realpath.
+script_path=$(dirname "$(realpath "$0")")
+
+# Change directory to $script_path.
+cd $script_path
+
+# Construct the full path to the script.
+alias_path="$script_path/$script_name"
+
+# -------------------------------------------------------------------------- #
+
+# Define functions_dir.
+functions_dir="$script_path/functions"
+
+# -------------------------------------------------------------------------- #
+
+# ========================================================================== #
+# C2 A9 2D 32 30 32 34 2D 4D 41 4E 5F 4D 41 44 45 5F 4D 41 54 45 52 49 61 4C #
+# ========================================================================== #
+
+# Changelist:       
+# -------------------------------------------------------------------------- #
+# version:               1.0.0
+# modified:              2024-04-20 - 16:20:00
+# comments:              refactored monolithic program into separate functions
+# -------------------------------------------------------------------------- #
+# version:               2.0.0
+# modified:              2024-04-29 - 11:29:27
+# comments:              testing production readiness
+# -------------------------------------------------------------------------- #
+# version:               2.0.1
+# modified:              2024-04-30 - 07:06:00
+# comments:              Removed 'declare -g' statements for macOS compatibility
+# -------------------------------------------------------------------------- #
+# version:               2.0.2
+# modified:              2024-04-30 - 12:29:07
+# comments:              added 'umask 0' statements for rsync commands
+# -------------------------------------------------------------------------- #
+# version:               2.0.3
+# modified:              2024-05-03 - 10:16:09
+# comments:              Restored CamelCase keys for projekt_metadata_xml_file
+# -------------------------------------------------------------------------- #
+# version:               2.0.4
+# modified:              2024-05-03 - 10:56:34
+# comments:              Restore 'jobs_dir' to /JOBS
