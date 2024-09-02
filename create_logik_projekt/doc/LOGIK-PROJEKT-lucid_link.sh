9781454f210df86cc5d570fc59910cc3fb65b800
phil-man-git-hub <phil_man@mac.com>
2024-09-01 17:00:45 -0700

    2024-08-31 initial release

--- create_logik_projekt/doc/LOGIK-PROJEKT-lucid_link.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create the /etc/synthetic.conf file
-sudo touch /etc/synthetic.conf
-
-# Enter the string 'JOBS Volumes/logik/salt/JOBS' into the file
-sudo bash -c "echo 'JOBS    Volumes/logik/salt/JOBS' >> /etc/synthetic.conf"
