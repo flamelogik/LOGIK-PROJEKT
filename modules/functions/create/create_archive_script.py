fa5d2bb2c38b23d2fdde24d46b3700d4cffb087c
phil-man-git-hub <phil_man@mac.com>
2024-12-27 11:09:48 -0800

    2024-12-27  fixed archiving script arguments

--- modules/functions/create/create_archive_script.py
+++ modules/functions/create/create_archive_script.py
@@ -189,6 +189,8 @@ separator = '# ' + '-' * 75 + ' #'
 
 # Function to create projekt flame archive script
 def create_projekt_flame_archive_script(
+        the_hostname,
+        the_projekt_os,
         the_projekts_dir,
         the_projekt_flame_dirs,
         the_adsk_dir,
@@ -196,7 +198,7 @@ def create_projekt_flame_archive_script(
         the_adsk_dir_macos,
         the_projekt_name,
         the_projekt_flame_name,
-        the_hostname,
+        the_sanitized_version,
         separator,
     ):
 
