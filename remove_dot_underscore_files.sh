2fd1339e7305f251c23e122a7104a2a9f4a7d5c7
ChadiABO <chadi@hecatstudio.com>
2025-09-29 16:39:21 +0200

    2025-09-29 update

--- remove_dot_underscore_files.sh
+++ remove_dot_underscore_files.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Recursively remove all files starting with ._ in the current directory and subdirectories
 
+set -ex
+
 find . -type f -name '._*' -print -delete
 
 echo "All ._ files have been removed."
